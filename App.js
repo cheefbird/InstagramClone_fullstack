3b94a47d67d9818ea0d4b99c7460a96aa6d2e24e
Francis Breidenbach <fbreidenbach@gmail.com>
2018-06-25 20:40:44 -0700

    Create and implement Card.

--- App.js
+++ App.js
@@ -2,18 +2,20 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Constants } from "expo";
 
-import AuthorRow from "./src/components/AuthorRow";
+// import AuthorRow from "./src/components/AuthorRow";
+import Card from "./src/components/Card";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <AuthorRow
+        <Card
           fullName={"First Last"}
           linkText={"Comments"}
           onPressLinkText={() => {
             console.log("LINK PRESSED!!!!!");
           }}
+          image={{ uri: "https://unsplash.it/600/600" }}
         />
       </View>
     );
